ca39b2a39d4d5d2214474afff188c8ee6a5cb9c1
Kaliztro <g.lopes1999@hotmail.com>
2022-01-06 21:31:56 -0300

    melhorias no codigo

--- commands/dono/restart.js
+++ commands/dono/restart.js
@@ -7,21 +7,23 @@ module.exports = {
     category: "dono",
     aliases: ["restart", "r"],
     run: async (client, message, args) => {
-        
-        if(message.author.id != config.donoID) {
+
+        if (message.author.id != config.donoID) {
             return message.channel.send(`${message.author}, Tu deve estar doidão né?, só o dono do Bot pode executar esse comando.`)
-          }
+        }
+
+        message.react("✅");
 
         const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args)); //importando o node-fetch (atualizado) / importing node-fetch (updated)
 
-        fetch(`https://discloud.app/status/bot/882715660134797342/restart`, { 
-            method: 'POST', 
+        fetch(`https://discloud.app/status/bot/882715660134797342/restart`, {
+            method: 'POST',
             headers: {
-                "api-token": config.Discloud 
+                "api-token": config.Discloud
             }
         }).then(info => info.json()).then(json => console.log(json));
-        
-        
+
+
 
     }
 }
