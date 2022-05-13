d4845af63b54190616137c68dd17ecd5bfbf94ff
kaliztro <g.lopes1999@hotmail.com>
2022-05-13 04:12:50 -0300

    melhorias

--- SlashCommands/config/config.js
+++ SlashCommands/config/config.js
@@ -34,15 +34,38 @@ module.exports = {
         const saida = interaction.options.getChannel('saida')
         const cargo = interaction.options.getRole('auto-cargo')
 
-        if (entrada) {
+        if (entrada && saida && cargo) {
             await Schema.findOneAndUpdate({ _id: interaction.guild.id }, { Welcome: { canal: entrada } }, { upsert: true });
-            interaction.reply({ content: `o Welcome definido foi: ${entrada}`, ephemeral: true })
-        } else if (saida) {
             await Schema.findOneAndUpdate({ _id: interaction.guild.id }, { Saida: { canal: saida } }, { upsert: true });
-            interaction.reply({ content: `o Saida definido foi: ${saida}`, ephemeral: true })
-        } else if (cargo) {
             await Schema.findOneAndUpdate({ _id: interaction.guild.id }, { AutoCargo: { cargo: cargo } }, { upsert: true });
-            interaction.reply({ content: `o cargo definido foi: ${cargo}`, ephemeral: true })
+            interaction.reply({ content: `o Welcome definido foi: ${entrada}, o Saida definido foi: ${saida}, o cargo definido foi: ${cargo} `, ephemeral: true });
+        }
+        else if (entrada && saida) {
+            await Schema.findOneAndUpdate({ _id: interaction.guild.id }, { Welcome: { canal: entrada } }, { upsert: true });
+            await Schema.findOneAndUpdate({ _id: interaction.guild.id }, { Saida: { canal: saida } }, { upsert: true });
+            interaction.reply({ content: `o Welcome definido foi: ${entrada}, o Saida definido foi: ${saida}`, ephemeral: true });
+        }
+        else if (entrada && cargo) {
+            await Schema.findOneAndUpdate({ _id: interaction.guild.id }, { Welcome: { canal: entrada } }, { upsert: true });
+            await Schema.findOneAndUpdate({ _id: interaction.guild.id }, { AutoCargo: { cargo: cargo } }, { upsert: true });
+            interaction.reply({ content: `o Welcome definido foi: ${entrada}, o cargo definido foi: ${cargo} `, ephemeral: true });
+        }
+        else if (saida && cargo) {
+            await Schema.findOneAndUpdate({ _id: interaction.guild.id }, { Saida: { canal: saida } }, { upsert: true });
+            await Schema.findOneAndUpdate({ _id: interaction.guild.id }, { AutoCargo: { cargo: cargo } }, { upsert: true });
+            interaction.reply({ content: `o Saida definido foi: ${saida}, o cargo definido foi: ${cargo} `, ephemeral: true });
+        }
+        else if (entrada) {
+            await Schema.findOneAndUpdate({ _id: interaction.guild.id }, { Welcome: { canal: entrada } }, { upsert: true });
+            interaction.reply({ content: `o Welcome definido foi: ${entrada}`, ephemeral: true });
+        }
+        else if (saida) {
+            await Schema.findOneAndUpdate({ _id: interaction.guild.id }, { Saida: { canal: saida } }, { upsert: true });
+            interaction.reply({ content: `o Saida definido foi: ${saida}`, ephemeral: true });
+        }
+        else if (cargo) {
+            await Schema.findOneAndUpdate({ _id: interaction.guild.id }, { AutoCargo: { cargo: cargo } }, { upsert: true });
+            interaction.reply({ content: `o cargo definido foi: ${cargo}`, ephemeral: true });
         }
 
     },
