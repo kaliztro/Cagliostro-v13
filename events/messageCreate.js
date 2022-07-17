fe9e732b472ecd9ef202e95bc7a2fcb6386bc3c6
kaliztro <g.lopes1999@hotmail.com>
2022-07-17 20:38:12 -0300

    mensagens randômicas  ao mencionar o bot

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -16,11 +16,17 @@ client.on("messageCreate", async (message) => {
     const emoj = client.guilds.cache.get("545386837846523905");
     const emoji = emoj?.emojis.cache.find(emoji => emoji.name === "cagliostro") || `👌`;
 
+    let user = message.author.username
+    let frases = [`olá ${user}`,`iai ${user}`, `coé ${user}`, `bah`, `Ué, quem é tu?`, `coé ${user}, pinga eu não`];
+
+    let rand = frases[Math.floor(Math.random()* frases.length)];
+
     const menção = message.mentions.users.first()
     if (menção) {
         if (menção.id === `${client.user.id}` && message.author.bot) { return message.channel.send(`Pinga eu não bot 🤬`) };
         if (menção.id === `${client.user.id}` && message.author.id === id.otelo) { return message.channel.send(`Pinga eu não nóia`)};
-        if (menção.id === `${client.user.id}`) { return message.channel.send(`${emoji}   Olá ${message.author.username}! Meu prefixo neste servidor é: **${prefix}**, Para mais informações utilize:  **${prefix}ajuda**`)}
+
+        if (menção.id === `${client.user.id}`) { return message.channel.send(`${emoji}  ${rand}! Meu prefixo neste servidor é: **${prefix}**, Para mais informações utilize:  **${prefix}ajuda**`)}
     };
 
 
